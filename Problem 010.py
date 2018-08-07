382257a1c9c788f5232143a40e6d3fccc8169f0c
Gizat <gizat.makhanov@gmail.com>
2018-08-07 22:55:09 +0600

    Optimize Problem 10

--- Problem 010.py
+++ Problem 010.py
@@ -1,45 +1,55 @@
 # Problem 10
 # Find the sum of all the primes below two million.
 
+import time
+
 
 def main():
 
-    number = 7
+    number = 1
 
-    prime_numbers = [2, 3, 5]
-
-    sum_of_primes = 0
+    prime_numbers = []
 
     while number < 2000000:
 
-        is_prime = True
-
-        if number % 2 == 0 or sum_digits(number) % 3 == 0 or number % 5 == 0:
-            is_prime = False
-        else:
-            for prime in prime_numbers:
-                if number % prime == 0:
-                    is_prime = False
-
-        if is_prime:
+        if is_prime(number):
             prime_numbers.append(number)
 
         number += 1
 
+    print(sum_primes(prime_numbers))
+
+
+def is_prime(n):
+    if n <= 1:
+        return False
+    elif n <= 3:
+        return True
+    elif n % 2 == 0 or n % 3 == 0:
+        return False
+
+    i = 5
+
+    while i * i <= n:
+        if n % i == 0 or n % (i + 2) == 0:
+            return False
+        i += 6
+
+    return True
+
+
+def sum_primes(prime_numbers):
+
+    sum_of_primes = 0
+
     for prime in prime_numbers:
         sum_of_primes += prime
 
-    print(sum_of_primes)
-
-
-def sum_digits(num):
-    r = 0
-
-    while num:
-        r, num = r + num % 10, num // 10
-
-    return r
+    return sum_of_primes
 
 
 if __name__ == '__main__':
+    start = time.time()
     main()
+    finish = time.time()
+    print("TOOK " + str(finish - start) + " SECONDS")
